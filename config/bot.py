5dd53b1cf61b01b4d4e92b0f01302b374b8c7be2
reirose <dead5root@gmail.com>
2022-03-31 18:55:52 +0300

    Выключил дебаг-режим :/

--- config/bot.py
+++ config/bot.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater
 
 import logging
 
-debug = 0
+debug = 0  # TODO: сделать командой бота
 TOKEN = "5240399182:AAFXRenIB9Sv9EisZBXlGIJEkbV9VxgajJc"
 DEBUG_TOKEN = "1050588160:AAHyzSPWCGn1Lw7OCRf9DeVCNuBp4hj5NQM"
 
